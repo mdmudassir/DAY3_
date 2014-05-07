f06b371781eaae052cbad0610a443a74848ed4b3
Mudassir <mudassir1033@gmail.com>
2014-05-07 16:28:38 -0400

    output

---
+++ output/output/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  output
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 User1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        char c, d ;
+        c='d';
+        d=c;
+        NSLog(@"d=%c", d);
+        
+    }
+    return 0;
+}
+
